61512059e0905f40aef614f987d426868a3e310a
marinamarshall <marinamarshall2@gmail.com>
2022-04-15 20:17:42 +0000

    Add database models

--- spa/models.py
+++ spa/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+BOOKED = ((0, "Unconfirmed"), (1, "Booked"))
+
+
+class Treatment(models.Model):
+    """ treatments """
+    featured_image = CloudinaryField('image', default='placeholder')
+    category = models.CharField(max_length=15)
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField()
+    time = models.DateTimeField(unique=True)
+
+    # class Meta:
+
+    def __str__(self):
+        return f"Treatment: {self.title}, booked for: {self.time}"
+
+
+class Client(models.Model):
+    """ client """
+    treatment = models.ForeignKey(
+        Treatment, on_delete=models.CASCADE, related_name="client_treatments")
+    client_id = models.IntegerField()
+    client_name = models.CharField(max_length=50)
+    client_email = models.EmailField(unique=True)
+    client_phone = models.IntegerField()
+
+    def __str__(self):
+        return self
+
+
+class Booking(models.Model):
+    """ booking """
+    booking_id = models.ForeignKey(
+        Client, on_delete=models.CASCADE, related_name="client_treatments")
+    booking_date = models.DateTimeField(unique=True)
+    booking_time = models.DurationField()
+    booked_status = models.IntegerField(choices=BOOKED, default=0)
+
+    def __str__(self):
+        return self
