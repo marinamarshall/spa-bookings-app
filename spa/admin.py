8b8cff109178668cd703653047ca57350d7847f5
marinamarshall <marinamarshall2@gmail.com>
2022-04-16 17:19:51 +0000

    Add ClientAdmin class to admin.py file

--- spa/admin.py
+++ spa/admin.py
@@ -6,7 +6,16 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Treatment)
 class TreatmentAdmin(SummernoteModelAdmin):
     list_display = ('category', 'title', 'slug', 'time')
+    list_filter = ('category', 'title')
     search_fields = ['category', 'title']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = 'description'
 
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'client_email', 'client_phone')
+    list_filter = ('client_name', 'client_email', 'client_phone')
+    search_fields = ['client_name', 'client_email', 'client_phone']
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
