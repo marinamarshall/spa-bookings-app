5dd1dd68881e0b991ca14b9d33219277a9316ed9
marinamarshall <marinamarshall2@gmail.com>
2022-04-16 17:08:03 +0000

    Add TreatmentAdmin class to admin.py file

--- spa/admin.py
+++ spa/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Treatment
+from .models import Treatment, Client, Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Treatment)
+
+@admin.register(Treatment)
+class TreatmentAdmin(SummernoteModelAdmin):
+    list_display = ('category', 'title', 'slug', 'time')
+    search_fields = ['category', 'title']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = 'description'
+
