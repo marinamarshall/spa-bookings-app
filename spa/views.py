47d3cdef291aa68d0f001f5f8cf48af74939dc9f
marinamarshall <marinamarshall2@gmail.com>
2022-04-16 19:38:07 +0000

    Add TreatmentList class in views.py file

--- spa/views.py
+++ spa/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Treatment
 
-# Create your views here.
+class TreatmentList(generic.ListView):
+    """ TreatmentList """
+    model = Treatment
+    queryset = Treatment.objects.filter(status=1)
+    template_name = 'index.html'
+    paginate_by = 9
