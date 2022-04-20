c31ff689d18576df5b5e41643dddc69a2ef3a817
marinamarshall <marinamarshall2@gmail.com>
2022-04-20 19:54:15 +0000

    Delete DB contents and make new migrations for appropriate DB schema

--- spa/admin.py
+++ spa/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Treatment)
 class TreatmentAdmin(SummernoteModelAdmin):
     """ TreatmentAdmin """
-    list_display = ('title', 'slug')
+    list_display = ('title', 'slug', 'category')
     list_filter = ('title',)
     search_fields = ['title']
     prepopulated_fields = {'slug': ('title',)}
@@ -16,17 +16,17 @@ class TreatmentAdmin(SummernoteModelAdmin):
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     """ ClientAdmin """
-    list_display = ('client_name', 'client_email', 'client_phone')
-    list_filter = ('client_name', 'client_email', 'client_phone')
-    search_fields = ['client_name', 'client_email', 'client_phone']
+    list_display = ('first_name', 'last_name', 'client_phone')
+    list_filter = ('first_name', 'last_name', 'client_phone')
+    search_fields = ['first_name', 'last_name', 'client_phone']
 
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     """ BookingAdmin """
-    list_display = ('booking_specification', 'booked_status')
-    list_filter = ('booking_specification', 'booked_status')
-    search_fields = ['booking_specification', 'booked_status']
+    list_display = ('booking_client', 'booking_treatment', 'booking_day', 'booking_time')
+    list_filter = ('booking_client', 'booking_treatment', 'booking_day', 'booking_time')
+    search_fields = ['booking_client', 'booking_treatment', 'booking_day', 'booking_time']
     actions = ['approve_booking']
 
     def approve_booking(self, request, queryset):
--- spa/models.py
+++ spa/models.py
@@ -7,40 +7,43 @@ BOOKED = ((0, "Unconfirmed"), (1, "Booked"))
 
 class Treatment(models.Model):
     """ treatments """
-    # featured_image = CloudinaryField('image', default='placeholder')
-    # category = models.CharField(max_length=15)
+    featured_image = CloudinaryField('image', default='placeholder')
     title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
+    category = models.CharField(max_length=15)
     description = models.TextField()
-    # time = models.DateTimeField(unique=True)
+    # time = models.DateTimeField()
 
     # class Meta:
 
     def __str__(self):
-        return f"Treatment: {self.title}, booked for: {self.time}"
+        return f"Treatment: {self.title} Added"
 
 
 class Client(models.Model):
     """ client """
-    # treatment = models.ForeignKey(
-    #     Treatment, on_delete=models.CASCADE, related_name="client_treatments")
     # client_id = models.IntegerField()
-    client_name = models.CharField(max_length=50)
-    client_email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
     client_phone = models.IntegerField()
+    is_staff = False
+    treatment_receiving = models.ForeignKey(
+        Treatment, on_delete=models.CASCADE, related_name="client_treatments")
 
     def __str__(self):
-        return f"New client added: {self.client_name} {self.client_phone} {self.client_email}"
+        return f"New client added: {self.first_name} {self.last_name}, {self.client_phone}, {self.email}"
 
 
 class Booking(models.Model):
     """ booking """
     # booking_id = models.ForeignKey(
     #     Client, on_delete=models.CASCADE, related_name="client_treatments")
-    # booking_treatment = models.ForeignKey(Treatment.title, on_delete=models.CASCADE)
-    booking_specification = models.DateTimeField(unique=True)
-    # booking_time = models.DurationField()
+    booking_client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    booking_treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+    booking_day = models.DateField()
+    booking_time = models.TimeField()
     booked_status = models.IntegerField(choices=BOOKED, default=0)
 
     def __str__(self):
-        return f"Your booking for {self.booking_specification} has been {self.booked_status}"
+        return f"{self.booking_client} has booked {self.booking_treatment} for {self.booking_day} at {self.booking_time}"
